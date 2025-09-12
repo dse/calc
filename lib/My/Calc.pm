17b174c257122eab0d8f392c68f611b3795c0650
Darren Embry <dse@webonastick.com>
2025-09-12 00:19:58 -0400

    ##, ###

--- lib/My/Calc.pm
+++ lib/My/Calc.pm
@@ -9,25 +9,50 @@ our @EXPORT = qw();
 our @EXPORT_OK = qw(calc_evaluate);
 our %EXPORT_TAGS = (all => \@EXPORT_OK);
 
-our $last_result = 0;
+our $last_numeric_result = 0;
+our $last_defined_result;
+our $last_result;
 
 use lib dirname(__FILE__) . "/../../lib";
 use My::Calc::Functions qw(:all);
 
+# prototype required for things like "_ + 5" to work
 sub _ () {
-    # prototype required for things like "_ + 5" to work
+    return $last_numeric_result;
+}
+sub __ () {
+    return $last_defined_result;
+}
+sub ___ () {
     return $last_result;
 }
 
 sub calc_evaluate {
     my ($expr, $fmt) = @_;
-    my $store_last_result = 1;
-    if ($expr =~ s/^\s*#\s*//) {
-        $store_last_result = 0;
+    my $store_numeric_result = 1;
+    my $store_defined_result = 0;
+    my $store_any_result     = 0;
+
+    if ($expr =~ s/^\s*#{3,}\s*//) {
+        # ### store any result including undef
+        $store_numeric_result = 1;
+        $store_defined_result = 1;
+        $store_any_result = 1;
+    } elsif ($expr =~ s/^\s*#{2}\s*//) {
+        # ## store any defined result
+        $store_numeric_result = 1;
+        $store_defined_result = 1;
+        $store_any_result = 0;
+    } elsif ($expr =~ s/^\s*#{1}\s*//) {
+        # # don't restore
+        $store_numeric_result = 0;
+        $store_defined_result = 0;
+        $store_any_result = 0;
     }
+
     if (looks_like_number($expr)) {
-        if ($store_last_result) {
-            $last_result = $expr;
+        if ($store_numeric_result || $store_defined_result || $store_any_result) {
+            $last_numeric_result = $expr;
         }
         if (!defined $fmt) {
             return $expr;
@@ -40,10 +65,18 @@ sub calc_evaluate {
         eval($expr);
     };
     if (!defined $result) {
+        if ($store_any_result) {
+            $last_result = $expr;
+        }
         return;
     }
-    if (looks_like_number($result) && $store_last_result) {
-        $last_result = $result;
+    if (looks_like_number($result)) {
+        if ($store_numeric_result) {
+            $last_numeric_result = $result;
+        }
+        if ($store_defined_result) {
+            $last_defined_result = $result;
+        }
     }
     if (defined $fmt) {
         $result = sprintf($fmt, $result);
